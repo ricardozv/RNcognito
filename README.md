a72a0b6a635b35cdc054d403c74f4b552181e6a3
zumb <zumb@zumb.zumb>
2022-06-19 02:41:38 -0300

    finish the login, use aws amplify

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RNcognito
 This app mobile build react native, aws amplify and cognito 
+
+App create witch expo, this todoAppExpo is 
+completed stack on login 
--- todoAppExpo/todoApp/App.js
+++ todoAppExpo/todoApp/App.js
@@ -1,14 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, View, StyleSheet, Text, Button, StatusBar } from 'react-native';
+import Amplify, { Auth } from 'aws-amplify';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import config from './src/aws-exports';
+import SignIn from './src/screens/SignIn';
+import SignUp from './src/screens/SignUp';
+import ConfirmSignUp from './src/screens/ConfirmSignUp';
+import Home from './src/screens/Home';
+
+Amplify.configure(config);
+
+const AuthenticationStack = createStackNavigator();
+const AppStack = createStackNavigator();
+const AuthenticationNavigator = props => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App</Text>
-      <StatusBar style="auto" />
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="SignIn">
+        {screenProps => (
+          <SignIn {...screenProps} updateAuthState={props.updateAuthState} />
+        )}
+      </AuthenticationStack.Screen>
+      <AuthenticationStack.Screen name="SignUp" component={SignUp} />
+      <AuthenticationStack.Screen
+        name="ConfirmSignUp"
+        component={ConfirmSignUp}
+      />
+    </AuthenticationStack.Navigator>
+  );
+};
+const AppNavigator = props => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="Home">
+        {screenProps => (
+          <Home {...screenProps} updateAuthState={props.updateAuthState} />
+        )}
+      </AppStack.Screen>
+    </AppStack.Navigator>
+  );
+};
+
+const Initializing = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" color="tomato" />
     </View>
   );
+};
+ 
+
+function App() {
+  const [isUserLoggedIn, setUserLoggedIn] = useState('initializing');
+
+  useEffect(() => {
+    checkAuthState();
+  }, []);
+  async function checkAuthState() {
+    try {
+      await Auth.currentAuthenticatedUser();
+      console.log('✅ User is signed in');
+      setUserLoggedIn('loggedIn');
+    } catch (err) {
+      console.log('❌ User is not signed in');
+      setUserLoggedIn('loggedOut');
+    }
+  }
+  {
+    return (
+      <NavigationContainer>
+        {isUserLoggedIn === 'initializing' && <Initializing />}
+        {isUserLoggedIn === 'loggedIn' && (
+          <AppNavigator updateAuthState={updateAuthState} />
+        )}
+        {isUserLoggedIn === 'loggedOut' && (
+          <AuthenticationNavigator updateAuthState={updateAuthState} />
+        )}
+      </NavigationContainer>
+    );
+  }
+  function updateAuthState(isUserLoggedIn) {
+    setUserLoggedIn(isUserLoggedIn);
+  }
+
+  async function signOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('Error signing out: ', error);
+    }
+  }
+
+  return (
+    
+    <View style={styles.container}>
+       <Text>💙 + 💛 = React Native + Amplify </Text>
+       <Button title="Sign Out" color="tomato" onPress={signOut} />
+      <StatusBar style="auto" />
+    </View>
+    
+  );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +113,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
